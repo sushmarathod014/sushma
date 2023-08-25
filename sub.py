6fbe7e0f9939bf50ee79337c715414b85796a0a3
sushma <sushmarathod014@gmail.com>
2023-08-25 15:56:40 +0530

    subtraction

---
+++ sub.py
@@ -0,0 +1,75 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 1,
+   "id": "2a7a751c",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "Result: 8\n"
+     ]
+    }
+   ],
+   "source": [
+    "def add(a, b):\n",
+    " return a + b\n",
+    "result = add(5, 3)\n",
+    "print(\"Result:\", result)"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 2,
+   "id": "d8cf3c25",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "Result: 2\n"
+     ]
+    }
+   ],
+   "source": [
+    "def sub(a, b):\n",
+    " return a - b\n",
+    "result = sub(5, 3)\n",
+    "print(\"Result:\", result)"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "daedd627",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3 (ipykernel)",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.11.3"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
